eac0391104c5ee00dacc831ddce795dd0e47d92c
Donabel Liotta <belramacho990@gmail.com>
2025-02-18 15:46:19 -0500

    added api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,24 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
     
     function search(event){
         event.preventDefault();
-    alert (`Searching for ${keyword}...`);
+
+    let apiKey = "e48bbeefa4cbc305ad9334597oa8t957";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
+
+    axios.get(apiUrl).then(handleResponse);
     }
+
 
 function handleKeywordChange(event){
     setKeyword(event.target.value);
@@ -18,7 +28,7 @@ function handleKeywordChange(event){
     return (
     <div className="Dictionary">
         <form onSubmit={search}>
-            <input className="Search" type="search" onChange={handleKeywordChange}/>
+            <input className="Search" type="search" placeholder="Search"  onChange={handleKeywordChange}/>
             </form>
             </div>
     );
