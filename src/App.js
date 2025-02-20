0246fb751729367e1ff428947bbb197479b472d9
Donabel Liotta <belramacho990@gmail.com>
2025-02-19 19:40:02 -0500

    added footer links

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ function App() {
 
      <Dictionary defaultKeyword="Dictionary"/>
 
-    <footer>Coded by 👩🏻‍💻 Donabel Liotta</footer>
+    <footer>This project was coded by 👩🏻‍💻 <a href="https://github.com/belram990" target="_blank" rel="noreferrer">Donabel Liotta</a> {" "}
+    and is open-sourced on <a href="https://github.com/belram990/dictionary-project" target="_blank" rel="noreferrer">GitHub</a>{" "}
+    and hosted on <a href="https://dl-dictionary-app.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.
+    </footer>
     </div>
     </div>
   );
